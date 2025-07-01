4181140d2c6b2bd81de0c3859023819fc87b68ae
Paul Scanlon <pauliescanlon@gmail.com>
2025-07-01 09:51:02 -0500

    test: change project name

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -12,8 +12,8 @@ export const mastra = new Mastra({
     level: "info"
   }),
   deployer: new CloudflareDeployer({
-    projectName: "hello-mastra",
-    scope: "",
+    projectName: "paul-dero-cloudflare",
+    scope: "a38ed3b8625e5f1e0b41e2c1c5aab2ff",
     auth: {
       apiToken: process.env.CLOUDFLARE_API_TOKEN!,
       apiEmail: "name@email.com"
